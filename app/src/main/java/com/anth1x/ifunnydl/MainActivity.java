7d15d0c370beaea0c5510e1e772eaedaf28b4625
anthop <33004321+anthony1x6000@users.noreply.github.com>
2023-07-04 16:11:35 -0400

    optimize imports

--- app/src/main/java/com/anth1x/ifunnydl/MainActivity.java
+++ app/src/main/java/com/anth1x/ifunnydl/MainActivity.java
@@ -10,7 +10,6 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
--- app/src/main/java/com/anth1x/ifunnydl/SettingsActivity.java
+++ app/src/main/java/com/anth1x/ifunnydl/SettingsActivity.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
