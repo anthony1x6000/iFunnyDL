d8e01fc452ca3d89ece5a18edb802839236834d8
anthop <33004321+anthony1x6000@users.noreply.github.com>
2023-07-04 16:24:13 -0400

    dont close app when downloading within app

--- app/src/main/java/com/anth1x/ifunnydl/MainActivity.java
+++ app/src/main/java/com/anth1x/ifunnydl/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
                     history.addURL(initShare);
 
                     startDLService(initShare);
-                    finishAndRemoveTask();
+                    // finishAndRemoveTask();
                 }
             });
         }
